30e7c3dfcffe5b06dde48fde03eaeab83c09f01a
arivu <arivu2rocks@gmail.com>
2020-01-24 18:05:15 +0900

    Internet speed test page work finished

--- user_agent/index.php
+++ user_agent/index.php
@@ -5,8 +5,6 @@
     <title>operation test of kaizen 2.0</title>
     <link rel="stylesheet" href="../assets/css/bootstrap/bootstrap.css">
     <link rel="stylesheet" href="../assets/css/style.css">
-    <link rel="stylesheet" href="../assets/css/landing-2.css">
-    <link rel="stylesheet" href="../assets/fonts/fontawesome/css/font-awesome.min.css">
 </head>
 
 <body>
@@ -15,16 +13,13 @@
         <!-- Material form login -->
         <div class="card mt-sm-5">
 
-            <h3 class="card-header custem-header info-color white-text text-center py-5"
-                style="color: white; background-color:#33b5e5;">
+            <h3 class="card-header custom-header info-color white-text text-center py-5">
                 <strong>For the operation test of kaizen 2.0 learning platform</strong>
             </h3>
 
             <!--Card content-->
             <div class="card-body px-lg-5 pt-0 mt-sm-5">
-            <div class="alert alert-danger" role="alert">
-	<strong>Oh snap!</strong> Change a few things up and try submitting again.
-</div>
+
                 <!-- Form -->
                 <form id="user_agent_form" action="thanks.php" onsubmit="return validation()" method="post"
                     class="text-center" style="color: #757575;" style="margin-top: 5% !important">
@@ -33,21 +28,23 @@
                         <!-- Company name -->
                         <div class="md-form">
                             <label>
-                                <h2><i class="fa fa-building pb_icon-gradient"></i> Company Name</h2>
+                                <h2><i class="fad fa-building fa-fw"></i> Company Name</h2>
                             </label>
                             <input type="text" class="form-control custom-input" id="companyName"
                                 name="companyName" placeholder="Enter Your Company Name Here">
                         </div>
                     </p>
+                    <br>
 
                     <!-- Internet speed -->
                     <div class="md-form">
                         <label>
-                          <h2><i class="fa fa-tachometer pb_icon-gradient"></i> Internet Speed</h2>
+                          <h2><i class="fad fa-tachometer-alt fa-fw"></i> Internet Speed</h2>
                           <div class="d-flex justify-content-around">
                               <div>
                                   <!-- link of internet speed  -->
-                                  <a href="http://fast.com" target="_blank"><h4><i class="fa fa-external-link pb_icon-gradient"></i> Click here to find out your Internet speed</h4></a>
+                                  <a href="http://fast.com" target="_blank"><h4><i class="fad fa-external-link-alt fa-fw"></i> Click here to find out your Internet speed</h4></a>
+                                  <p class="par-font">Note:After clicking the link above, Please wait some moments for getting the exact internet speed.</p>
                               </div>
                           </div>
                         </label>
@@ -70,12 +67,12 @@
                     </div><br>
 
                     <!-- Submit button -->
-                    <button class="btn btn-outline-info btn-rounded btn-block btn-font my-4 waves-effect z-depth-0" type="submit"
-                        id="register" value="Save" name="submit"><i class="fa fa-paper-plane pb_icon-gradient"></i> Submit</button>
+                    <button class="btn btn-primary btn-rounded btn-block btn-font mt-5 mb-4 waves-effect z-depth-0" type="submit"
+                        id="register" value="Save" name="submit"><i class="fad fa-paper-plane fa-fw"></i> Submit</button>
                 </form>
                 <!-- Warning message -->
-                <div class="alert alert-danger" style="display:none">
-                    <button type="button" class="close" aria-hidden="true">&times;</button>
+                <div class="alert alert-danger alert-font" style="display:none">
+                    <button type="button" class="close d-none" aria-hidden="true">&times;</button>
                     please fill out all the information.
                 </div>
 
@@ -84,10 +81,10 @@
 
         <script src="../assets/js/jquery.min.js"></script>
         <script src="../assets/js/bootstrap.min.js"></script>
+        <script defer src="../assets/css/awesome.min.js"></script>
         
         <script>
-
-        $(document).on('click', "button.close", function(e) { // Actually live method is absolute for latest Jquery 
+            $(document).on('click', "button.close", function(e) { // Actually live method is absolute for latest Jquery 
             e.preventDefault(); // This is for safe if there any default behavior
             $(this).parent().hide();
         });
